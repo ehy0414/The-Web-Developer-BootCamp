d3fa39e37ce468730eeab4044016c5cb1bf99c0a
ehy0414 <djagusdyd78@gmail.com>
2025-11-30 16:40:34 +0900

    section18 수강

---
+++ section18/app.js
@@ -0,0 +1,47 @@
+
+const person = {
+    firstName: 'Mick',
+    lastName: 'Jagger'
+}
+
+const kitchenSink = {
+    favNum: 92312,
+    isFunny: true,
+    colors: ['red', 'orange']
+}
+
+console.log(person.firstName);
+console.log(person["firstName"]);
+console.log(kitchenSink.colors[0]);
+
+const midterms = {
+    dnielle: 96,
+    thomas: 78
+};
+
+midterms.thomas = 79;
+
+console.log(midterms);
+
+const comments = [
+    {
+        username: 'sss',
+        text: 'dlool',
+        votes: 9
+    },
+    {
+        username: 'fish',
+        text: 'glub',
+        votes: 12387
+    }
+];
+
+
+
+
+
+
+
+
+
+
