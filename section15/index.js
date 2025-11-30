f20534fb69f047fe734a842495a27bf30029d225
ehy0414 <djagusdyd78@gmail.com>
2025-11-30 13:47:03 +0900

    section15 수강

---
+++ section15/index.js
@@ -0,0 +1,10 @@
+let msg = " dsdas "
+
+msg = msg.toUpperCase();
+
+
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5).replace("o", "e");
+
