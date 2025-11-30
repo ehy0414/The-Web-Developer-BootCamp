e2806443370fc30aa6d35abf97c5d85b2be10f0e
ehy0414 <djagusdyd78@gmail.com>
2025-11-30 12:46:21 +0900

    section14 JavaScript 수강

---
+++ section14/index.js
@@ -0,0 +1,18 @@
+let a = 10;
+a++;
+console.log(a);
+
+const b = 20;
+b++;
+console.log(b);
+
+let _age = 0;
+
+// 카멜케이스
+let currentDate;
+
+// 스네이크
+let current_date;
+
+// 변수명은 카멜케이스로 작성하는 것을 선호함
+// 변수명은 의미와 연관지어 이해하기 쉽게 선언해야 함
