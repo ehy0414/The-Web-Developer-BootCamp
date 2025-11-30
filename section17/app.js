0b2fce7e1575bd5a3b1a9f09540741d45cc97553
ehy0414 <djagusdyd78@gmail.com>
2025-11-30 16:16:59 +0900

    section17 수강

---
+++ section17/app.js
@@ -0,0 +1,30 @@
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr[2]);
+
+// arr.push(7);
+
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(arr.shift(0));
+// console.log(arr);
+
+// console.log(arr.unshift(10));
+// console.log(arr);
+
+// let arr2 = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr.concat(arr2));
+// arr2.includes(2); //true
+
+const PI = 3.141592;
+PI += 1 //Error
+
+const nums = [1, 2, 3];
+
+const gameBoard = [['X', 'O', 'X'], ['O', null, 'X'], ['X', 'O', null]];
+
+
+
